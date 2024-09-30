9e258f51006f29c93533bf6aef8edca4d4b8afd2
stevejcl <stevejcl@users.noreply.github.com>
2024-09-30 12:13:32 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
 DWARF_IP = ""
+DWARF_ID = ""
+DWARF_UI = ""
 CLIENT_ID = "0000DAF2-0000-1000-8000-00805F9B34FB"
+# LOG_FILE for log file, app.log if ommitted or empty
+LOG_FILE = ""
+# "" to omit debug level information
 DEBUG = True
+# "" to omit info level information
+TRACE = False
