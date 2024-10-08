abe3cf65dc507529d209bc091548894aa908edad
stevejcl <stevejcl@users.noreply.github.com>
2024-10-08 22:45:26 +0200

    edit config ini files and config.py

--- config.py
+++ config.py
@@ -2,10 +2,13 @@ DWARF_IP = ""
 DWARF_ID = ""
 DWARF_UI = ""
 CLIENT_ID = "0000DAF2-0000-1000-8000-00805F9B34FB"
-# LOG_FILE for log file, app.log if ommitted or if empty redirect to console
-LOG_FILE = "test.log"
+# TIMEOUT_CMD for TimeOut deconnection (s) after not receiving command
+# none or ommited set by defaut to 30 minutes
+# set to 0 to remove Timeout : None forced deconnection
+TIMEOUT_CMD = "0"
+# LOG_FILE for log file, app.log if ommitted or empty; "False" to redirect to console
+LOG_FILE = ""
 # True to add debug level information on log file, if not will be Info Level
-DEBUG = True
-# "" to omit info level information or True
+DEBUG = "True"
 # True to add info level information on console, if not will be Notice Level
-TRACE = ""
+TRACE = "False"
--- program.py
+++ program.py
@@ -13,8 +13,9 @@ from lib.dwarf_utils import parse_ra_to_float
 from lib.dwarf_utils import parse_dec_to_float
 from lib.dwarf_utils import perform_takeAstroPhoto
 
+###################
 # JSON ENTRY FILE
-
+###################
     programm = 
         { command : {
                 id_command : {
@@ -55,98 +56,107 @@ from lib.dwarf_utils import perform_takeAstroPhoto
                 }
             }
         }
+################
+# define
+################
+
+error_action = 
+
+def init_session(programm)
 
 
 def start_session(programm)
+    try:
+        calibration = programm.calibration.do_action
+        goto_solar = programm.goto_solar.do_action
+        if (goto_solar)
+            target_name = programm.goto_solar.target
+        goto_manual = programm.goto_manual.do_action
+        if (goto_manual)
+            manual_RA = programm.goto_manual.ra_coord
+            manual_declination = programm.goto_manual.dec_coord
+            target_name = programm.goto_manual.target
 
-    calibration = programm.calibration.do_action
-    goto_solar = programm.goto_solar.do_action
-    if (goto_solar)
-        target_name = programm.goto_solar.target
-    goto_manual = programm.goto_manual.do_action
-    if (goto_manual)
-        manual_RA = programm.goto_manual.ra_coord
-        manual_declination = programm.goto_manual.dec_coord
-        target_name = programm.goto_manual.target
 
+        take_photo = programm.setup_camera.do_action
+        if (take_photo)
+             if(programm.setup_camera.exposure)
+                 exp_val = programm.setup_camera.exposure
 
-    take_photo = programm.setup_camera.do_action
-    if (take_photo)
-         if(programm.setup_camera.exposure)
-             exp_val = programm.setup_camera.exposure
+             if(programm.setup_camera.gain)
+                 gain_val = programm.setup_camera.gain
+     
+             if(programm.setup_camera.binning)
+                 binning_val = programm.setup_camera.binning
 
-         if(programm.setup_camera.gain)
-             gain_val = programm.setup_camera.gain
- 
-         if(programm.setup_camera.binning)
-             binning_val = programm.setup_camera.binning
+             if(programm.setup_camera.IRCut)
+                 IR_val = programm.setup_camera.IRCut
 
-         if(programm.setup_camera.IRCut)
-             IR_val = programm.setup_camera.IRCut
+             if(programm.setup_camera.count)
+                 count_val = programm.setup_camera.count
 
-         if(programm.setup_camera.count)
-             count_val = programm.setup_camera.count
+        # init camera are always done at startup
+        # TO DO add error_control
+        continu_action = True
+        perform_time()
+        if (read_longitude() and read_lattitude())
+            perform_timezone()
+        else 
+            stop_action()
 
-    # init camera are always done at startup
-    # TO DO add error_control
-    continu_action = True
-    perform_time()
-    if (read_longitude() and read_lattitude())
-        perform_timezone()
-    else 
-        stop_action()
-
-    continu_action = perform_GoLive()
-    verify_action(continu_action, "step_1")
-
-    continu_action = permform_update_camera_setting("exposure", "1")
-    verify_action(continu_action, "step_1")
-    continu_action = permform_update_camera_setting("gain", "80")
-    verify_action(continu_action, "step_1")
-    continu_action = permform_update_camera_setting("IR", "1")
-    verify_action(continu_action, "step_1")
-    continu_action = permform_update_camera_setting("binning", "0")
-    verify_action(continu_action, "step_1")
-
-    if (calibration)
-        time.sleep(programm.calibration.wait_before)
-        continu_action = perform_calibration()
-        time.sleep(programm.calibration.wait_after)
-    verify_action(continu_action, "step_1")
-
-    if (goto_solar)
-        continu_action = select_solar_target (target): 
-        time.sleep(programm.goto_solar.wait_after)
-    verify_action(continu_action, "step_1")
-
-    if (goto_manual)
-        try:
-            decimal_RA = float(manual_RA)
-        except ValueError:
-            decimal_RA = parse_ra_to_float(manual_RA)
-        try:
-            decimal_Dec = float(manual_declination)
-        except ValueError:
-            decimal_Dec = parse_ra_to_float(manual_declination)
-        perform_goto(decimal_RA, decimal_Dec, target_name)
-        time.sleep(programm.goto_manual.wait_after)
-    verify_action(continu_action, "step_1")
-
-    if (setup_photo)
-        if (exp_val)
-            permform_update_camera_setting("exposure", exp_val)
-        if (exposure)
-            permform_update_camera_setting("gain", gain_val)
-        if (IR_val)
-            permform_update_camera_setting("IR", IR_val)
-        if (binning_val)
-            permform_update_camera_setting("binning", binning_val)
-        if (count_val)
-            permform_update_camera_setting("count", count_val)
-        time.sleep(programm.setup_camera.wait_after)
-
+        continu_action = perform_GoLive()
         verify_action(continu_action, "step_1")
 
-        perform_takeAstroPhoto()   
-
+        continu_action = permform_update_camera_setting("exposure", "1")
         verify_action(continu_action, "step_1")
+        continu_action = permform_update_camera_setting("gain", "80")
+        verify_action(continu_action, "step_1")
+        continu_action = permform_update_camera_setting("IR", "1")
+        verify_action(continu_action, "step_1")
+        continu_action = permform_update_camera_setting("binning", "0")
+        verify_action(continu_action, "step_1")
+
+        if (calibration)
+            time.sleep(programm.calibration.wait_before)
+            continu_action = perform_calibration()
+            time.sleep(programm.calibration.wait_after)
+        verify_action(continu_action, "step_1")
+
+        if (goto_solar)
+            continu_action = select_solar_target (target): 
+            time.sleep(programm.goto_solar.wait_after)
+        verify_action(continu_action, "step_1")
+
+        if (goto_manual)
+            try:
+                decimal_RA = float(manual_RA)
+            except ValueError:
+                decimal_RA = parse_ra_to_float(manual_RA)
+            try:
+                decimal_Dec = float(manual_declination)
+            except ValueError:
+                decimal_Dec = parse_ra_to_float(manual_declination)
+            perform_goto(decimal_RA, decimal_Dec, target_name)
+            time.sleep(programm.goto_manual.wait_after)
+        verify_action(continu_action, "step_1")
+
+        if (setup_photo)
+            if (exp_val)
+                permform_update_camera_setting("exposure", exp_val)
+            if (exposure)
+                permform_update_camera_setting("gain", gain_val)
+            if (IR_val)
+                permform_update_camera_setting("IR", IR_val)
+            if (binning_val)
+                permform_update_camera_setting("binning", binning_val)
+            if (count_val)
+                permform_update_camera_setting("count", count_val)
+            time.sleep(programm.setup_camera.wait_after)
+
+            verify_action(continu_action, "step_1")
+
+            perform_takeAstroPhoto()   
+
+            verify_action(continu_action, "step_1")
+
+    except :
